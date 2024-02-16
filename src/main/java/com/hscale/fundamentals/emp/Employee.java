4439fa401af6e4ca93a17e23307120f61ed06c86
Abhishek Kumar <abhi553@gmail.com>
2024-02-16 23:12:29 +0000

    coding fundamentals project

---
+++ src/main/java/com/hscale/fundamentals/emp/Employee.java
@@ -0,0 +1,71 @@
+package com.hscale.fundamentals.emp;
+
+/**
+ * @author Abhishek Kumar on 16/02/2024
+ */
+public class Employee {
+  private   String empFirstname;
+   private String empLastname;
+   private long empId;
+    private int age;
+   private String department;
+
+    public Employee(String empFirstname, String empLastname, long empId, int age, String department) {
+        this.empFirstname = empFirstname;
+        this.empLastname = empLastname;
+        this.empId = empId;
+        this.age = age;
+        this.department = department;
+    }
+
+    public void setEmpFirstname(String empFirstname) {
+        this.empFirstname = empFirstname;
+    }
+
+    public void setEmpLastname(String empLastname) {
+        this.empLastname = empLastname;
+    }
+
+    public void setEmpId(long empId) {
+        this.empId = empId;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getEmpFirstname() {
+        return empFirstname;
+    }
+
+    public String getEmpLastname() {
+        return empLastname;
+    }
+
+    public long getEmpId() {
+        return empId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empFirstname='" + empFirstname + '\'' +
+                ", empLastname='" + empLastname + '\'' +
+                ", empId=" + empId +
+                ", age=" + age +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/hscale/fundamentals/emp/Main.java
@@ -0,0 +1,17 @@
+package com.hscale.fundamentals.emp;
+
+/**
+ * @author Abhishek Kumar on 16/02/2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Guriya","Kumari",1234,31,"IT");
+
+        Employee emp2 = new Employee("Margo","Singh",119,3,"PlayGroup");
+
+        System.out.println(emp);
+        System.out.println("\n===========\n");
+        System.out.println(emp2);
+
+    }
+}
